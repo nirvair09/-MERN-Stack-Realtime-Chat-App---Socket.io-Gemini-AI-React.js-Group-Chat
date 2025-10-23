01ffe6b2171a8adf0fa4d40965cb03e0b0c54944
nirvair09 <chandradeep262003@gmail.com>
2025-10-23 21:49:39 +0530

    chatModel added

---
+++ server/models/chat.model.ts
@@ -0,0 +1,56 @@
+import mongoose ,{Document,Schema} from "mongoose";
+
+
+export interface ChatDocument extend Document{
+    participants:mongoose.Types.ObjectId[];
+    lastMessage:mongoose.Types.ObjectId;
+    isGroup:boolean;
+    createdBy:mongoose.Types.ObjectId;
+    createdAt:Date;
+    updatedAt:Date;
+
+}
+
+const chatSchema = new Schema<ChatDocument>(
+    {
+        participants:[
+            {
+                types:Schema.Types.ObjectId,
+                ref:"User",
+                required:true,
+            },
+        ],
+        lastMessage:[
+            {
+                types:Schema.Types.ObjectId,
+                ref:"Message",
+                default:null,
+            }
+        ],
+        isGroup:[
+            {
+                type:Schema.Types.ObjectId,
+                default:false,
+            }
+        ],
+        groupName:[
+            {
+                type: String,
+            }
+        ],
+        createdBy:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:"User",
+                required:true,
+            }
+        ],
+        
+    },
+    {
+        timestamps:true,
+    }
+);
+
+const ChatModel=mongoose.model<ChatDocument>("Chat",chatSchema);
+export default ChatModel;
